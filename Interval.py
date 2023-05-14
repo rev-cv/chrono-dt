e12df26dda1d807c54325373ebd1603f845d5d54
rev-cv <MrRomanRev@gmail.com>
2023-05-14 18:46:53 +0500

    Interval.py

---
+++ Interval.py
@@ -0,0 +1,204 @@
+
+import datetime
+from ch_in_catchers import setRoundOff, setInterval
+from ch_in_validators import (
+    isReal,
+    isDateWithinInterval,
+    isIntervalWithinInterval,
+    isTupleInterval
+)
+from Chrono import Chrono
+from ch_in_mutators import chgToStepInterval
+from IntervalsGenerate import (
+    merge2Intervals,
+    fragmentByDay,
+    fragmentByDecade,
+    fragmentByMonth,
+    fragmentByWeek,
+    fragmentByQuarter,
+    fragmentByYear,
+    fragmentByDecennary,
+    fragmentByHour,
+    fragmentByMinute,
+    fragmentByIntervals,
+    subtractByIntervals,
+    convertIntervals,
+)
+
+class Interval(object):
+    def __init__(self, dates = None, roundoff = None, expansion=True):
+        super(Interval, self).__init__()
+
+        self.s = None  # start начало временного интервала
+        self.f = None  # finish конец временного интервала
+        self.tz = None # временная зона периода
+
+        self.roundoff = setRoundOff(roundoff)
+        self.expansion = expansion
+
+        self.set(dates)
+    
+
+    # SETTERS ↲
+
+    def set(self, dates):
+        if type(dates) is tuple or type(dates) is list:
+            if isTupleInterval(dates):
+                # интервал получен как ((1970, 1, 1, 0, 0, 0), (1970, 1, 25, 0, 0, 0))
+                self.s, self.f = setInterval(dates[0], dates[1], self.roundoff, self.expansion)
+            elif isinstance(dates[0], Chrono) and isinstance(dates[1], Chrono):
+                # интервал получен как (Chrono, Chronon)
+                if dates[0].tz != dates[1].tz:
+                    dates[1].toTimeZone(dates[0].tz)
+                self.s, self.f = setInterval(
+                    (dates[0].y, dates[0].m, dates[0].d, dates[0].H, dates[0].M, dates[0].S),
+                    (dates[1].y, dates[1].m, dates[1].d, dates[1].H, dates[1].M, dates[1].S),
+                    self.roundoff, self.expansion
+                )
+        elif isinstance(dates, Interval):
+            # интервал получен как Interval
+            self.s, self.f = list(*dates.s), list(*dates.f)
+            self.tz = dates.tz
+            self.roundoff = dates.roundoff
+            self.expansion = dates.expansion
+        elif isinstance(dates, Chrono):
+            self.s, self.f = setInterval(
+                (dates.y, dates.m, dates.d, dates.H, dates.M, dates.S),
+                dates, None, self.roundoff, self.expansion
+            )
+            self.tz = dates.tz
+        else:
+            self.s, self.f = setInterval(dates, None, self.roundoff, self.expansion)
+
+
+    # QUASTIONS ↲
+
+    # ↓ является ли полученный интервал реальным?
+    def isReal(self):
+        return isReal(self.s, self.f, self.expansion, isGenerateError=False)
+    
+    # ↓ входит ли дата или аргумент в данный интервал?
+    def isIncluded(self, arg = None, isFullEntry=False):
+
+        # ↓ если передан период
+        if (type(arg) is tuple or type(arg) is list) and len(arg) == 2:
+            return isIntervalWithinInterval(self.s, self.f, arg[0], arg[1], isFullEntry)
+        elif isinstance(arg, Interval):
+            return isIntervalWithinInterval(self.s, self.f, arg.s, arg.f, isFullEntry)
+        
+        # ↓ если передана дата
+        else:
+            d = Chrono(arg)
+            return isDateWithinInterval(self.s, self.f, d.getDateTime(), isFullEntry)
+    
+
+    # MUTATTORS ↲
+
+    # ↓ next / prev интервал в зависимости от заданного roundoff и шагов
+    def step(self, st=0):
+        self.s, self.f = chgToStepInterval(self.s, st, self.roundoff)
+        return self
+
+    # ↓ объединяет два интервала
+    def join(self, i2, greedy = False):
+        if (type(i2) is tuple or type(i2) is list) and len(i2) == 2:
+            self.s, self.f = merge2Intervals(self.s, self.f, i2[0], i2[1], greedy)
+        elif isinstance(i2, Interval):
+            self.s, self.f = merge2Intervals(self.s, self.f, i2.s, i2.f, greedy)
+        else:
+            i2 = Interval(i2)
+            self.s, self.f = merge2Intervals(self.s, self.f, i2.s, i2.f, greedy)
+        return self
+    
+    # ↓ объединяет два интервала, если между ними нет пропуска
+    def __truediv__(self, other): # ( / )
+        return self.join(i2 = other, greedy = False)
+
+    # ↓ объединяет два интервала даже если между ними есть пропуск
+    # пропуск становится частью нового интервала
+    def __floordiv__(self, other): # ( // )
+        return self.join(i2 = other, greedy = True)
+    
+    
+    # GENERATOR NEW INTERVALS ↲
+
+    # ↓ фрагментирует данный интервал на более мелкие интервалы 
+    def fragment(self, intervals="day") -> list:
+        if type(intervals) is str:
+            # передано указание, на какие интервалы дробить данный интервал
+            if "day" == intervals:
+                return fragmentByDay(self.s, self.f)
+            elif "month" == intervals:
+                return fragmentByMonth(self.s, self.f)
+            elif "week" == intervals:
+                return fragmentByWeek(self.s, self.f)
+            elif "qua" == intervals[:3]: # quarter
+                return fragmentByQuarter(self.s, self.f)
+            elif "year" == intervals:
+                return fragmentByYear(self.s, self.f)
+            elif "dece" == intervals[:4]: # decennary
+                return fragmentByDecennary(self.s, self.f)
+            elif "dec" == intervals[:3]: # decade
+                return fragmentByDecade(self.s, self.f)
+            elif "hour" == intervals:
+                return fragmentByHour(self.s, self.f)
+            elif "min" == intervals[:3]: # minute
+                return fragmentByMinute(self.s, self.f)
+        elif type(intervals) is tuple or type(intervals) is list and len(intervals) > 0:
+            # переданные интервалы будут вписаны в self-интервал (обрезаны и удалены)
+            intervals = convertIntervals(intervals)
+            return fragmentByIntervals(self.s, self.f, intervals)
+        return list()
+
+    # ↓ «вычитает» из self-интервала переданные интервалы
+    # возвращает интервалы оставшиеся после «вычитания» 
+    def subtract(self, intervals) -> list:
+        intervals = convertIntervals(intervals)
+        return subtractByIntervals(self.s, self.f, intervals)
+
+
+
+    def start(self):
+        return Chrono(*self.s)
+
+    def finish(self):
+        return Chrono(*self.f)
+    
+    def dts(self):
+        return datetime.datetime(*self.s)
+
+    def dtf(self):
+        return datetime.datetime(*self.f)
+    
+    def qdts(self):
+        pass
+
+    def qdtf(self):
+        pass
+
+
+    def __str__(self) -> str:
+        return f'{self.s[0]}-{self.s[1]}-{self.s[2]} — {self.f[0]}-{self.f[1]}-{self.f[2]}'
+    
+    
+
+if __name__ == '__main__':
+    # a = Interval("2023-05-12", 'dece')
+    # aa = a.fragment("dec")
+
+    # for x in aa:
+    #     print(x)
+
+    # print(len(aa))
+
+    bb = subtractByIntervals(
+        [2023, 5, 14, 0, 0, 0],
+        [2023, 5, 15, 0, 0, 0],
+        [ 
+            [ [2023, 5, 14, 15, 0, 0], [2023, 5, 14, 18, 0, 0]],
+            [ [2023, 5, 14, 9, 0, 0], [2023, 5, 14, 10, 0, 0]]
+        ]
+    )
+    
+    for x in bb:
+        print(x)
--- ch_in_validators.py
+++ ch_in_validators.py
@@ -1,4 +1,4 @@
-
+import datetime
 
 def isReal(start, finish, isExpansion = True, isGenerateError=False):
     # является ли полученный интервал start—finish реальным?
@@ -6,6 +6,12 @@ def isReal(start, finish, isExpansion = True, isGenerateError=False):
     # то такого периода не может существовать в реальности.
 
     # Функция isReal принимает объекты Chrono и datetime.datetime
+
+    if type(start) is list or type(start) is tuple:
+        start = datetime.datetime(*start)
+    
+    if type(finish) is list or type(finish) is tuple:
+        finish = datetime.datetime(*finish)
     
     if start >= finish:
         if isGenerateError:
@@ -24,10 +30,21 @@ def isReal(start, finish, isExpansion = True, isGenerateError=False):
             error += "There may be an error in the received `class Interval` data."
             # Возможна ошибка в полученных данных `class Interval`.
             raise Exception(error)
+        return False
+    return True
 
 def isDateWithinInterval(date, start, finish):
     # входит ли переданная дата в данный период?
-    # Функция isDateWithinInterval принимает объекты Chrono и datetime.datetime
+
+    if type(start) is list or type(start) is tuple:
+        start = datetime.datetime(*start)
+    
+    if type(finish) is list or type(finish) is tuple:
+        finish = datetime.datetime(*finish)
+    
+    if type(date) is list or type(date) is tuple:
+        date = datetime.datetime(*date)
+    
     if start <= date <= finish:
         return True
     return False
@@ -40,6 +57,18 @@ def isIntervalWithinInterval(ds, df, in_ds, in_df, isFullEntry=False):
     # isFullEntry — 
     # должно ли быть полное вхождение (True) 
     # или интервал может выходить за границы (False)?
+
+    if type(ds) is list or type(ds) is tuple:
+        ds = datetime.datetime(*ds)
+    
+    if type(df) is list or type(df) is tuple:
+        df = datetime.datetime(*df)
+    
+    if type(in_ds) is list or type(in_ds) is tuple:
+        in_ds = datetime.datetime(*in_ds)
+    
+    if type(in_df) is list or type(in_df) is tuple:
+        in_df = datetime.datetime(*in_df)
 
     if isFullEntry is False:
         if in_ds <= ds < df <= in_df:
@@ -62,3 +91,41 @@ def isIntervalLess(ds1, df1, ds2, df2):
     if df1 - ds1 < df2 - ds2:
         return True
     return False
+
+
+def isTupleInterval(interval):
+    # проверяет, является ли интервал типа ((1970, 1, 1, 0, 0, 0), (1970, 1, 25, 0, 0, 0))
+    isTuple = True
+    
+    if type(interval) is not tuple or type(interval) is not list:
+        isTuple = False
+
+    if len(interval) != 2:
+        isTuple = False
+
+    if type(interval[0]) is not tuple or type(interval[0]) is not list:
+        isTuple = False
+
+    if len(interval[0]) != 6:
+        isTuple = False
+
+    for x in interval[0]:
+        if type(x) is not int:
+            isTuple = False
+            break
+    
+    if type(interval[1]) is not tuple or type(interval[1]) is not list:
+        isTuple = False
+
+    if len(interval[1]) != 6:
+        isTuple = False
+
+    for x in interval[1]:
+        if type(x) is not int:
+            isTuple = False
+            break
+    
+    return isTuple
+        
+    
+
